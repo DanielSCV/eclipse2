05c7d38c84bdffa0e51ad5b60f8a216bca764d8d
Daniel Cardenas <dcv00@iesemilidarder.com>
2024-02-15 19:24:38 +0100

    Primera version

---
+++ src/paquete/HolaMundo3.java
@@ -0,0 +1,10 @@
+package paquete;
+
+public class HolaMundo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("¡Hola, mundo! Que tal?");
+	}
+
+}
